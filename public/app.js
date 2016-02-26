378963eea6e65953dc09a0702941f667b7c181dd
Lauxx <lm.nichols@hotmail.com>
2016-02-26 13:02:24 -0700

    our challenge this week

--- public/app.js
+++ public/app.js
@@ -1,29 +1,38 @@
-{/* 
-Need to ininitialize the state of products 
-Need to set the state of products
-*/} 
+console.log("hello");
+
+
 
 var ProductTable = React.createClass({
-  propTypes: {
-    url: React.PropTypes.string.isRequired,
+  getInitialState: function(){
+    return {
+      products: []
+    }
   },
+
   loadProductsFromServer: function() {
     var self = this;
     $.ajax({
       url: this.props.url,
       method: 'GET'
     }).done(function(data){
-      {/* A JSX comment */}
-      {/* Your code here */}
-    })
+      this.setState({
+        products: data
+      })
+    }.bind(this));
   },
+
+  propTypes: {
+    url: React.PropTypes.string.isRequired,
+  },
+
   componentDidMount: function() {
     this.loadProductsFromServer();
   },
+
   render: function() {
     return (
       <div>
-        <ProductList products={this.state.SOMETHING} />
+        <ProductList products={this.state.products} />
       </div>
       )
   }
@@ -34,31 +43,43 @@ var ProductTable = React.createClass({
 Filter through products and map only products in stock..
 Replace the table body section with dynamic data.
 */}  
-
+var filterProduct = function(product){
+  return product.inStock = true;
+};
 var  ProductList = React.createClass({
   render: function() {
-        return (
+    var self = this;
+    var productList = this.props.products.filter(filterProduct).map(function(product){
+       
+        return(
         <div>
           <table className="table table-striped">
             <thead>
               <tr>
-                <th>product</th>
-                <th>cost </th>
-                <th> inStock </th>
+                <th>Name of Product</th>
+                <th>Cost of Product</th>
+                <th>In Stock Product</th>
               </tr>
             </thead>
             <tbody>
             <tr>
-              <td> each name </td>
-              <td> each cost </td>
-              <td> inStock is true </td>
+              <td> {product.name} </td>
+              <td> {product.cost} </td>
+              <td> {product.inStock.toString()} </td>
             </tr>
             </tbody>
           </table>
         </div>
+          )
+        
+      })
+    return(
+      <div>
+        {productList}
+      </div>
       )
   }
 });
 
-React.render(<ProductTable />, document.getElementById('react-container'));
+React.render(<ProductTable url = '/api/products'/>, document.getElementById('react-container'));
 {/* WHICH URL IS USED TO GET ALL PRODUCTS? */}
